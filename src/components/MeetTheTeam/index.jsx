fd25c56c99ae8992b403dcc486eb72d42ad92640
mahcodes <mhmdali102@bk.ru>
2023-01-09 04:14:37 +0200

    implemented contributions count as @luxus sugessted

--- src/components/MeetTheTeam/index.jsx
+++ src/components/MeetTheTeam/index.jsx
@@ -1,13 +1,9 @@
 import React, { useEffect, useState } from "react";
 import styles from "./styles.modules.css";
-import Link from "@docusaurus/Link";
-import Stars from "@site/src/components/Stars";
 
 const TeamMembersList = [
   {
-    name: "Christian Chiarulli",
     username: "ChristianChiarulli",
-    img: "https://avatars.githubusercontent.com/u/29136904?v=4",
     donate: [
       { name: "GitHub Sponsors", link: "https://github.com/sponsors/ChristianChiarulli" },
       { name: "Patreon", link: "https://www.patreon.com/chrisatmachine" },
@@ -19,26 +15,40 @@ const TeamMembersList = [
   },
   {
     username: "kylo252",
-    img: "https://avatars.githubusercontent.com/u/59826753?v=4",
   },
   {
-    name: "Abouzar Parvan",
     username: "abzcoding",
-    img: "https://avatars.githubusercontent.com/u/10992695?v=4",
   },
   {
     username: "rebuilt",
-    img: "https://avatars.githubusercontent.com/u/16025007?v=4",
   },
 ];
 
 const TeamMembers = () => {
+  const [contributors, setContributors] = useState({});
+
+  useEffect(() => {
+    fetch("https://api.github.com/repos/LunarVim/LunarVim/contributors")
+      .then((res) => res.json())
+      .then((data) => {
+        const contributors_list = data.map((contributor) => ({
+            [contributor.login]: {
+            name: contributor.login,
+            img: contributor.avatar_url,
+            username: contributor.login,
+            contributions: contributor.contributions,
+          }}
+        ));
+        setContributors(Object.assign({}, ...contributors_list));
+      }).catch((err) => console.error(err));
+  }, []);
+
   return (
     <section>
       <div id="team-members" className={`container ${styles.container}`}>
         <div className={styles.members}>
           {TeamMembersList.map((member) => (
-            <TeamMember key={member.username} {...member} />
+            <TeamMember key={member.username} donate={member.donate} {...contributors[member.username]} />
           ))}
         </div>
       </div>
@@ -47,7 +57,8 @@ const TeamMembers = () => {
 };
 
 
-const TeamMember = ({ name, username, img, bio, donate }) => {
+const TeamMember = ({ name, username, img, bio, contributions,donate }) => {
+  console.log(username)
   const nameEl = (name ? <span>{name}</span> : null);
   const usernameEl = (<a href={`https://github.com/${username}`}>@{username}</a>);
   const donationsEl = (donate ? (
@@ -66,12 +77,10 @@ const TeamMember = ({ name, username, img, bio, donate }) => {
     <div className={styles.member}>
       {img ? <img src={img} alt="profile pic" /> : null}
       <h3>{nameEl} {usernameEl}</h3>
-
+      <p>{contributions} contributions</p>
       <p>{bio ? bio : "Write something about yourself."}</p>
-
       <div style={{ clear: "both" }}> </div>
       {donationsEl}
-
     </div >
   );
 };
